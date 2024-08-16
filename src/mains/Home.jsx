f4d7a66569d0e53393bdf47da3d96898a4930366
Tibo <tibo.verreycken@telenet.be>
2024-08-17 00:11:38 +0200

    update

--- src/mains/Home.jsx
+++ src/mains/Home.jsx
@@ -1,12 +1,20 @@
 import Header from "../components/Header.jsx";
 import Nav from "../components/Nav.jsx";
+import styled from "styled-components";
+
+
+const StyledP = styled.p`
+    width: 80%;
+    margin: 0 auto;
+`;
 
 export default function Home() {
     return (
         <>
             <Header title={"Homepage"}/>
             <Nav/>
-            <p>blah 1</p>
+            <br/>
+            <StyledP>blah 1</StyledP>
         </>
     );
 }
