c65ec24d9ac138a63baac5c4e878354db463419e
Tibo <tibo.verreycken@telenet.be>
2024-11-09 20:31:53 +0100

    change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Personal-2024-Summer-Resume'
+  build: {
+    outDir: 'docs'
+  },
+  base: 'klanting.github.io/Personal-2024-Summer-Resume'
 })
